98b33826149f0ab1528ccf636a7b3f9ced0f12ec
asifzcpe <rahmanasif05004@gmail.com>
2024-09-28 01:33:01 +0600

    chore: remove unwanted code

--- src/providers/AutoFactoryServiceProvider.php
+++ src/providers/AutoFactoryServiceProvider.php
@@ -6,8 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use Asif\AutoFactory\Console\Commands\GenerateFactoryCommand;
 use Asif\AutoFactory\Contracts\FieldMapperInterface;
 use Asif\AutoFactory\Services\FakerFieldMapper;
-use DB;
-use Log;
 
 class AutoFactoryServiceProvider extends ServiceProvider
 {
@@ -19,14 +17,6 @@ class AutoFactoryServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            DB::listen(function ($query) {
-                Log::info(
-                    $query->sql,
-                    $query->bindings,
-                    $query->time
-                );
-            });
-
             $this->commands([
                 GenerateFactoryCommand::class,
             ]);
